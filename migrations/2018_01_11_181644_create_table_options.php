893d7dcdc49138bac3c6f4ef16b1ed8e177bce8f
mradang <mradang@163.com>
2022-05-02 20:00:53 +0800

    适配 laravel 9

--- migrations/2018_01_11_181644_create_table_options.php
+++ migrations/2018_01_11_181644_create_table_options.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableOptions extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -30,4 +30,4 @@ class CreateTableOptions extends Migration
     {
         Schema::dropIfExists('options');
     }
-}
+};
